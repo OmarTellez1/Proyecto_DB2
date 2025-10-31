6202c46870784b781f285cff27e2e4e844c5bdd9
Omar <omartellezse@gmail.com>
2025-10-31 03:48:52 -0500

    Agregue middleware en userRoutes y productRoutes

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller.js';
-
+import { authMiddleware } from '../middleware/auth.middleware.js';
 const router = Router();
 
 // GET /api/productos/
 router.get('/', ProductController.getAll);
 
 // POST /api/productos/
-router.post('/', ProductController.create);
+router.post('/', authMiddleware,ProductController.create);
 
 // GET /api/productos/:id
 router.get('/:id', ProductController.getById);
 
 // PUT /api/productos/:id
-router.put('/:id', ProductController.update);
+router.put('/:id', authMiddleware,ProductController.update);
 
 // --- NUEVA RUTA ---
 // DELETE /api/productos/:id
-router.delete('/:id', ProductController.remove);
+router.delete('/:id', authMiddleware,ProductController.remove);
 // -----------------
 
 export default router;
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,27 +1,27 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller.js';
-
+import { authMiddleware } from '../middleware/auth.middleware.js';
 const router = Router();
 
 // POST /api/usuarios/
-router.post('/', UserController.create);
+router.post('/',authMiddleware, UserController.create);
 
 // GET /api/usuarios/
-router.get('/', UserController.getAll);
+router.get('/', authMiddleware,UserController.getAll);
 
 // GET /api/usuarios/:id
-router.get('/:id', UserController.getById);
+router.get('/:id', authMiddleware,UserController.getById);
 
 // --- NUEVA RUTA ---
 // PUT /api/usuarios/:id
-router.put('/:id', UserController.update);
+router.put('/:id',UserController.update);
 // -----------------
 
 
 // --- RUTA DELETE (ACTUALIZADA/AÑADIDA) ---
 // DELETE /api/usuarios/:id
 // Esto conecta la URL con la función del controlador
-router.delete('/:id', UserController.remove);
+router.delete('/:id', authMiddleware,UserController.remove);
 // -----------------
 
 export default router;
